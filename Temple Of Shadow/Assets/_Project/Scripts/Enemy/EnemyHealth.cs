72d5729132eea74940ca5dc8ecf0d6de361c98f3
Thuanle27 <76564695+levinhthuanle@users.noreply.github.com>
2026-06-17 19:32:42 +0700

    Thuanlv/Feat: Add Enemy Logic

--- Temple Of Shadow/Assets/_Project/Scripts/Enemy/EnemyHealth.cs
+++ Temple Of Shadow/Assets/_Project/Scripts/Enemy/EnemyHealth.cs
@@ -1,37 +1,89 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class EnemyHealth : MonoBehaviour
 {
+    private Animator animator;
+    private EnemyAI enemyAI;
+    private EnemyAttack enemyAttack;
+
     [SerializeField] private int maxHp = 5;
+    [SerializeField] private float destroyDelay = 1f;
 
     private int currentHp;
-    private Animator animator;
+    private bool isDeadOrNot = false;
 
-    private static readonly int hurt = Animator.StringToHash("Hurt");
+    private static readonly int Hurt = Animator.StringToHash("hurt");
+    private static readonly int Die = Animator.StringToHash("die");
+    private static readonly int IsDead = Animator.StringToHash("isDead");
 
     private void Awake()
     {
         currentHp = maxHp;
+
         animator = GetComponentInChildren<Animator>();
+        enemyAI = GetComponent<EnemyAI>();
+        enemyAttack = GetComponent<EnemyAttack>();
     }
 
     public void TakeDamage(int damage)
     {
-        currentHp -= damage;   
+        if (isDeadOrNot) return;
 
+        currentHp -= damage;
 
         Debug.Log($"{gameObject.name} took {damage} damage. HP left: {currentHp}");
-        Hurt();
+
         if (currentHp <= 0)
         {
-            Destroy(gameObject);
+            DieEnemy();
+            return;
+        }
+
+        animator.SetTrigger(Hurt);
+
+        if (enemyAI != null)
+        {
+            enemyAI.SetCanMove(false);
+        }
+
+        if (enemyAttack != null)
+        {
+            enemyAttack.ResetAttackState();
         }
     }
 
-    public void Hurt()
+    public void FinishHurt()
     {
-        animator.SetTrigger(hurt);
+        if (enemyAI != null && !isDeadOrNot)
+        {
+            enemyAI.SetCanMove(true);
+        }
+    }
+
+    private void DieEnemy()
+    {
+        isDeadOrNot = true;
+
+        animator.SetBool(IsDead, true);
+        animator.SetTrigger(Die);
+
+        if (enemyAI != null)
+            enemyAI.enabled = false;
+
+        if (enemyAttack != null)
+            enemyAttack.enabled = false;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = Vector2.zero;
+
+        Collider2D col = GetComponent<Collider2D>();
+        col.enabled = false;
+
+        Destroy(gameObject, destroyDelay);
+    }
+
+    public bool IsDeadYet()
+    {
+        return isDeadOrNot;
     }
 }
