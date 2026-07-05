5ff30bb02bd7d4a6587ac3a26a21428bd5010a60
Thuanle27 <76564695+levinhthuanle@users.noreply.github.com>
2026-07-05 23:20:45 +0700

    thuanlv/Feat: Now we can loot item

--- Temple Of Shadow/Assets/_Project/Scripts/Collectable/LootItem.cs
+++ Temple Of Shadow/Assets/_Project/Scripts/Collectable/LootItem.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 public class LootItem : MonoBehaviour
 {
     public ItemData itemData;
+    private InventoryManager inventoryManager;
 
     [SerializeField]
     private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        inventoryManager = FindFirstObjectByType<InventoryManager>();
+
+    }
 
     public void Initialize(ItemData item)
     {
         itemData = item;
         spriteRenderer.sprite = item.icon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        inventoryManager.AddItem(itemData);
+
+        Destroy(gameObject);
     }
 }
